ade887f35ad5b873052851d8cb1af76290f25f25
Samzy Black <blacksamzy386@gmail.com>
2026-09-25 08:39:44 +0100

    acdgacduai

--- routes/teams.js
+++ routes/teams.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Team = require('../models/Team');
+const Team = require('../models/Teams');
 const Match = require('../models/Match');
 
 const router = express.Router();
